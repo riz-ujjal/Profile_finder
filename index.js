b8b8da7eb26708233838c70028826e43cce600aa
riz-ujjal <rizujjal@gmail.com>
2026-06-25 09:33:59 +0530

    added the users details to fetch in the website

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const user_input = document.querySelector("#username-input");
 const search_button = document.querySelector("#search-btn");
 const display_name = document.querySelector("#name-display");
-const diaplay_bio = document.querySelector("#bio-display");
+const display_bio = document.querySelector("#bio-display");
 const count_follower = document.querySelector("#followers-count");
 const count_following = document.querySelector("#following-count");
 const count_repos = document.querySelector("#repo-count")
-
+// /add avatar which is image 
+const user_avatar = document.querySelector("#avatar")
 search_button.addEventListener("click", async () =>{
     const user = user_input.value;
-    const response = await fetch("https://api.github.com/user/google");
-    const data = await response.json();
-    
+    try{
+        const response = await fetch("https://api.github.com/user/google");
+        const data = await response.json();
+        display_name.textContent = data.name;
+        display_bio.textContent = data.bio;
+        count_follower.textContent = data.follower;
+        count_following.textContent = data.following;
+        count_reops.textContent = data.repos;
+        user_avatarElement.src = data.avatar_url;
+    }
+    catch(error){
+        display_name.textContent = "User Not found!";
+    }
 })
