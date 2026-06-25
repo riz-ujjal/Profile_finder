6ca29de8433e984b430e8e2023938ae01538eb6e
riz-ujjal <rizujjal@gmail.com>
2026-06-25 09:17:10 +0530

    storing user deatils in javascript

---
+++ index.js
@@ -0,0 +1,7 @@
+const user_input = document.querySelector("#username-input");
+const search_button = document.querySelector("#search-btn");
+const display_name = document.querySelector("#name-display");
+const diaplay_bio = document.querySelector("#bio-display");
+const count_follower = document.querySelector("#followers-count");
+const count_following = document.querySelector("#following-count");
+const count_repos = document.querySelector("#repo-count")
